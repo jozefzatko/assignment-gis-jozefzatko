5f7b59a6574f25007f7e356a427338ba3962649d
Jozef Zatko <jozefzatko.sk@gmail.com>
2015-10-25 01:03:57 +0200

    Added seed of freshwater

--- gis_rails_app/db/seeds.rb
+++ gis_rails_app/db/seeds.rb
@@ -7,15 +7,20 @@
 include ActiveModel::Serializers::JSON
 require 'json'
 
-puts "Making data seed. It may takes a few seconds. Please wait..."
+puts "Making data seed. It may takes a few minutes. Please wait..."
 
 connection = ActiveRecord::Base.connection()
+
+##############################################################################################################
 
 countries_file 				= "/home/jozef/rails_projects/assignment-gis-jozefzatko/data/countries.json"
 feow_hydrosheds_file 	= "/home/jozef/rails_projects/assignment-gis-jozefzatko/data/feow_hydrosheds.json"
 freshwater_ecoregions = "/home/jozef/rails_projects/assignment-gis-jozefzatko/data/freshwater_ecoregions.txt"
+freshwater_file_1 		= "/home/jozef/rails_projects/assignment-gis-jozefzatko/data/glwd_1.json"
+freshwater_file_2 		= "/home/jozef/rails_projects/assignment-gis-jozefzatko/data/glwd_2.json"
+freshwater_file_3 		= "/home/jozef/rails_projects/assignment-gis-jozefzatko/data/glwd_3.json"
 
-### Countries ##########################################################################################
+### Countries ################################################################################################
 
 data_hash = JSON.parse(File.read(countries_file))
 
@@ -39,7 +44,7 @@ end
 connection.execute(" UPDATE countries SET json_coordinates = replace(json_coordinates, '=>', ':'); ")
 connection.execute(" UPDATE countries SET coordinates=ST_SetSRID(st_geomfromgeojson(json_coordinates),4326); ")
 
-### Freshwater Ecoregions ##############################################################################
+### Freshwater Ecoregions ####################################################################################
 
 ecoregions_data = Array.new
 i = 0
@@ -74,3 +79,92 @@ end
 
 connection.execute(" UPDATE freshwater_ecoregions SET json_coordinates = replace(json_coordinates, '=>', ':'); ")
 connection.execute(" UPDATE freshwater_ecoregions SET coordinates=ST_SetSRID(st_geomfromgeojson(json_coordinates),4326); ")
+
+### Freshwater no. 1 #########################################################################################
+
+puts "Still seeding..."
+
+data_hash = JSON.parse(File.read(freshwater_file_1))
+
+data_hash['features'].each do |freshwtr|
+
+	Freshwater.create(
+		
+		feow_id: 							freshwtr["properties"]["FEOW_ID"],
+		name: 								freshwtr["properties"]["LAKE_NAME"],
+		area_km2:							freshwtr["properties"]["AREA_SKM"],
+		perimeter_km:					freshwtr["properties"]["PERIM_KM"],
+		longitude:						freshwtr["properties"]["LONG_DEG"],
+		latitude:							freshwtr["properties"]["LAT_DEG"],
+		elevation:						freshwtr["properties"]["ELEV_M"],
+		country:							freshwtr["properties"]["COUNTRY"],
+		secondary_countries:	freshwtr["properties"]["SEC_CNTRY"],
+		river:								freshwtr["properties"]["RIVER"],
+		near_city:						freshwtr["properties"]["NEAR_CITY"],
+		
+		json_coordinates:			freshwtr["geometry"].to_s
+		
+		) unless freshwtr["geometry"].nil?
+	
+end
+
+### Freshwater no. 2 #########################################################################################
+
+data_hash = JSON.parse(File.read(freshwater_file_2))
+
+data_hash['features'].each do |freshwtr|
+
+	Freshwater.create(
+		
+		feow_id: 							freshwtr["properties"]["FEOW_ID"],
+		name: 								freshwtr["properties"]["LAKE_NAME"],
+		area_km2:							freshwtr["properties"]["AREA_SKM"],
+		perimeter_km:					freshwtr["properties"]["PERIM_KM"],
+		longitude:						freshwtr["properties"]["LONG_DEG"],
+		latitude:							freshwtr["properties"]["LAT_DEG"],
+		elevation:						freshwtr["properties"]["ELEV_M"],
+		country:							freshwtr["properties"]["COUNTRY"],
+		secondary_countries:	freshwtr["properties"]["SEC_CNTRY"],
+		river:								freshwtr["properties"]["RIVER"],
+		near_city:						freshwtr["properties"]["NEAR_CITY"],
+		
+		json_coordinates:			freshwtr["geometry"].to_s
+		
+		) unless freshwtr["geometry"].nil?
+	
+end
+
+### Freshwater no. 3 #########################################################################################
+
+puts "Be patient..."
+
+#data_hash = JSON.parse(File.read(freshwater_file_3))
+
+#data_hash['features'].each do |freshwtr|
+
+#	Freshwater.create(
+		
+#		feow_id: 							freshwtr["properties"]["FEOW_ID"],
+#		name: 								freshwtr["properties"]["LAKE_NAME"],
+#		area_km2:							freshwtr["properties"]["AREA_SKM"],
+#		perimeter_km:					freshwtr["properties"]["PERIM_KM"],
+#		longitude:						freshwtr["properties"]["LONG_DEG"],
+#		latitude:							freshwtr["properties"]["LAT_DEG"],
+#		elevation:						freshwtr["properties"]["ELEV_M"],
+#		country:							freshwtr["properties"]["COUNTRY"],
+#		secondary_countries:	freshwtr["properties"]["SEC_CNTRY"],
+#		river:								freshwtr["properties"]["RIVER"],
+#		near_city:						freshwtr["properties"]["NEAR_CITY"],
+		
+#		json_coordinates:			freshwtr["geometry"].to_s
+		
+#		) unless freshwtr["geometry"].nil?
+	
+#end
+
+connection.execute(" UPDATE freshwaters SET json_coordinates = replace(json_coordinates, '=>', ':'); ")
+connection.execute(" UPDATE freshwaters SET coordinates=ST_SetSRID(st_geomfromgeojson(json_coordinates),4326); ")
+
+puts "Seeding complete."
+
+##############################################################################################################
