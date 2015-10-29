8e0ba170d07aa7c06f5c9f1efee32f684625c2a0
Jozef Zatko <jozefzatko.sk@gmail.com>
2015-10-29 01:28:16 +0100

    Updated Freshwater model and controller to show data on map

--- gis_rails_app/app/controllers/freshwaters_controller.rb
+++ gis_rails_app/app/controllers/freshwaters_controller.rb
@@ -1,7 +1,20 @@
 class FreshwatersController < ApplicationController
 
   def index
-    @freshwaters = Freshwater.all
+ 
+    @geojson_data = Array.new
+		
+    @freshwaters = Freshwater.where("freshwater_type = ?","River").limit(200)
+    @freshwaters += Freshwater.where("id < ?",200)
+		
+		@freshwaters.each do |freshwater|
+			
+      @geojson_data << freshwater.get_geojson
+			
+		end
+
+		gon.geo_data = @geojson_data
+    
   end
   
   def show
--- gis_rails_app/app/models/freshwater.rb
+++ gis_rails_app/app/models/freshwater.rb
@@ -1,2 +1,68 @@
 class Freshwater < ActiveRecord::Base
+  
+  def get_marker
+    if freshwater_type == "Lake"  
+      @marker = "water"
+      else
+      if freshwater_type == "Reservoir"
+        @marker = "dam"
+      else 
+        if freshwater_type == "River"
+          @marker = "ferry"
+        else
+          @marker = "marker"
+        end
+      end
+    end
+  end
+  
+  
+  def get_tooltip
+    @tooltip =  "<b>Area:</b> " + get_area.to_s + "km2<br><b>Country:</b> " + get_country.to_s + "<br><b>Type:</b> " + get_type.to_s
+  end
+  
+  
+  def get_area
+    if area_km2.nil?
+      @area = "-"
+    end
+    @area = area_km2.to_s
+  end
+  
+  
+  def get_country
+    if country.nil?
+      @country = "-"
+    end
+    @country = country
+  end
+  
+  
+  def get_type
+    if freshwater_type.nil?
+      @type = "-"
+    end
+    @type = freshwater_type.to_s
+  end
+  
+  
+  def get_geojson
+    
+    @geojson = {
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [longitude,latitude]
+    			},
+    			"properties": {
+						"title": name,
+            "description": get_tooltip,
+      				"marker-color": "#fc4353",
+      				"marker-size": "large",
+            "marker-symbol": get_marker
+			}
+		}
+    
+  end
+  
 end
